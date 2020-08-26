d5ab78de077c69a433a56eeee04aca7bb1cb3bc2
dnsgk4983 <64947285+dnsgk4983@users.noreply.github.com>
2020-08-26 19:02:06 +0900

    826 19시

--- js/script.js
+++ js/script.js
@@ -16,48 +16,88 @@ $(function(){
 });
 
 // 공지사항 롤링 애니메이션
-$(document).ready(function(){
-    var height = $(".notice").height();
-    var num = $('.rolling li').length;
-    var max = height * num;
-    var move = 0;
-    function noticeRolling(){
-        move += height;
-        $('.rolling').animate({"top":-move},600,function(){
-            if ( move > 1 ){
-                $(this).css("top",-244);
-                move = 0;
-            };
-        });
-    };
-    noticeRollingOff = setInterval(noticeRolling,5000);
-    $('.rolling').append($('.rolling li').first().clone());
+jQuery(function($)
+{
+    var ticker = function()
+    {
+        timer = setTimeout(function(){
+            $('#ticker li:first').animate( {marginTop: '-61px'}, 500, function()
+            {
+                $(this).detach().appendTo('ul#ticker').removeAttr('style');
+            });
+            ticker();
+        }, 2000);         
+      };
+// 0번 이전 기능
+      $(document).on('click','.prev',function(){
+        $('#ticker li:last').hide().prependTo($('#ticker')).slideDown();
+        clearTimeout(timer);
+        ticker();
+        if($('#pause').text() == 'Unpause'){
+          $('#pause').text('Pause');
+        };
+      }); // 0번 기능 끝
+  
+// 1. 클릭하면 다음 요소 보여주기... 클릭할 경우 setTimeout 을 clearTimeout 해줘야 하는데 어떻게 하지..
+      $(document).on('click','.next',function(){
+            $('#ticker li:first').animate( {marginTop: '-61px'}, 400, function()
+                    {
+                        $(this).detach().appendTo('ul#ticker').removeAttr('style');
+                    });
+            clearTimeout(timer);
+            ticker();
+            //3 함수와 연계 시작
+            if($('#pause').text() == 'Unpause'){
+              $('#pause').text('Pause');
+            }; //3 함수와 연계
+          }); // next 끝. timer 를 전연변수보다 지역변수 사용하는게 나을 것 같은데 방법을 모르겠네요.
 
-    // $(".rolling_stop").click(function(){
-    //     clearInterval(noticeRollingOff);
-    // }); // 롤링 멈추는 소스
-    // $(".rolling_start").click(function(){
-    //     noticeRollingOff = setInterval(noticeRolling, 1000);
-    // }); // 롤링 재시작 소스
-
-    $('.prev').click(function(){
-        move += height ;
-        $('.rolling').animate({"top":-move},function(){
-            if (move >= max){
-                $(this).css("top",0);
-                move = 0;
+  //2. 재생정지기능 시작, 아직 다음 기능과 연동은 안됨...그래서 3을 만듦
+  var autoplay = true;
+      $(document).on('click','.pause',function(){
+            if(autoplay==true){
+              clearTimeout(timer);
+              $(this).text('재생');
+              autoplay=false;
+            }else{
+              autoplay=true;
+              $(this).text('정지');
+              ticker();
             }
-        });
+          }); // 재생정지기능 끝  
+  // 3. 재생정지 함수 시작. 2와 기능 동일함.
+    var tickerpause = function()
+  {
+    $('#pause').click(function(){
+      $this = $(this);
+      if($this.text() == 'Pause'){
+        $this.text('Unpause');
+        clearTimeout(timer);
+      }
+      else {
+        ticker();
+        $this.text('Pause');
+      }
     });
-    $('.next').click(function(){
-        move -= height;
-        $('.rolling').animate({"top":-move},function(){
-            if (move == 61){
-                $('.rolling').append($('.rolling li').last().clone());
-            }
-        });
-
+   
+  };
+  tickerpause();
+  //3 재생정지 함수 끝
+  //4 마우스를 올렸을 때 기능 정지
+  var tickerover = function()
+  {
+    $('#ticker').mouseover(function(){
+      clearTimeout(timer);
     });
+    $('#ticker').mouseout(function(){
+      ticker();
+    });  
+  };
+  tickerover();
+  // 4 끝
+    ticker();
+    
+});
 
     // 주요모델 슬라이드
     $('.condition-pagination li:nth-child(1)').click(function(){
@@ -102,8 +142,7 @@ $(document).ready(function(){
         $('.iphone-SE-wrap').fadeIn();
     });
     
-    
-});
+
 
 
 // 차트 영역
